b1d4908d4d1470948118fd65c5c1cb6c84faeed5
Colin Densem <colin@summit360.co.uk>
2015-05-17 09:25:07 +0100

    Updated config, release fails without Ecto config

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -7,11 +7,11 @@ config :summit360_www, Summit360Www.Endpoint,
   secret_key_base: System.get_env("SUMMIT360_WWW_SECRET_KEY_BASE")
 
 # # Configure your database
-# config :summit360_www, Summit360Www.Repo,
-#   adapter: Ecto.Adapters.Postgres,
-#   username: "postgres",
-#   password: "postgres",
-#   database: "production",
-#   size: 20 # The amount of database connections in the pool
+config :summit360_www, Summit360Www.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: System.get_env("SUMMIT360_WWW_PG_USERNAME"),
+  password:System.get_env("SUMMIT360_WWW_PG_PASSWORD"),
+  database: System.get_env("SUMMIT360_WWW_PG_DATABASE"),
+  size: 20 # The amount of database connections in the pool
 
 
